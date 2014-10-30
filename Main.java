ec6aa95842bb21a0f8add1771f861cab1a84cc44
Kim <kakakan@live.se>
2014-10-30 20:58:35 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,11 @@
 package com.company;
-import javafx.util.converter.IntegerStringConverter;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         function function = new function(); // Här säger jag till att den ska läsa ifrån Person.java
-        function.Function();
         function.counter();
 
     }
