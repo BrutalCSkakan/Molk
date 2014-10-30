793d7b63c7384a323c08b7221e0954eba064dd0a
Kim <kakakan@live.se>
2014-10-30 20:27:48 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.company;
+import javafx.util.converter.IntegerStringConverter;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        function function = new function(); // Här säger jag till att den ska läsa ifrån Person.java
+        function.Function();
+        function.testing();
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
