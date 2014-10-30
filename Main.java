456b984c020b04b425b6db059630a0adee37d831
Kim <kakakan@live.se>
2014-10-30 20:43:00 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,14 +3,15 @@ import javafx.util.converter.IntegerStringConverter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
         function function = new function(); // Här säger jag till att den ska läsa ifrån Person.java
         function.Function();
-        function.testing();
+        function.counter();
 
     }
 
