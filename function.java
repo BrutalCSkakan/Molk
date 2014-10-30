217bf0b969eaab1fb6d11fe4fc40de55d348e684
Kim <kakakan@live.se>
2014-10-30 20:42:46 +0100

    Update function.java

--- function.java
+++ function.java
@@ -1,46 +1,56 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class function {
+String file = "C:\\Users\\kakak_000\\IdeaProjects\\untitled5\\src\\com\\company\\lexikon.txt";
 
-    int words = 0;
+    public int counter() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
 
-    public void Function() throws FileNotFoundException {
+        int count = 0;
+        while ((br.readLine()) != null) {
+            count++;
+        }
+        br.close();
+        return count;
+    }
 
-        File text = new File("C:\\Users\\kakak_000\\IdeaProjects\\untitled5\\src\\com\\company\\lexikon.txt"); 
-        Scanner scnr = new Scanner(text); 
+    public void Function() throws IOException {
+
+        File text = new File(file); // Jag gör en fil i mitt program med namnet "text", som tar info ifrån filen "C:\\Users\\kakak_000\\IdeaProjects\\untitled5\\src\com\\\company\\lexikon.txt"
+        Scanner scnr = new Scanner(text); // Döper min scanner som ska läsa av filen "text"
         int i;
-        HashMap<String, String> lexikon; 
-        lexikon = new HashMap<String, String>(); 
+        HashMap<String, String> lexikon; // Jag "skapar" oldlistan/HashMap'en "lexikon"
+        lexikon = new HashMap<String, String>(); // Jag berättar att min ordlista lexikon ska ta två Strings
         System.out.print("Skriv in ett värde i consolen: ");
-        for (i = 0; i < testing(); i++) { 
+        for (i = 0; i < counter(); i++) { // En for loop som loopar igenom alla rader i lexikon.txt
 
 
-            String line = scnr.nextLine(); 
-            String[] arr = line.split("\\s+"); 
-            arr[0] = arr[0].toLowerCase(); 
-            arr[1] = arr[1].toLowerCase();
+            String line = scnr.nextLine(); // Jag förenklar funktionen "scnr.nextLine()" till "line" och tillåter även ett värde att matas in i consolen
+            String[] arr = line.split("\\s+"); // Här delar jag upp raden i 2 arrays(arr[0] och arr[1], tack vare mellanslaget som är mellan orden
+            arr[0] = arr[0].toLowerCase(); // Jag säger till arr[0] att skrivas in i ordlistan med små bokstäver. Detta är endast för att underlätta så man slipper tänka på att skriva orden med stor bokstav osv
+            arr[1] = arr[1].toLowerCase();// Jag säger till arr[1] att skrivas in i ordlistan med små bokstäver. Detta är endast för att underlätta så man slipper tänka på att skriva orden med stor bokstav osv
 
-            lexikon.put(arr[0].toLowerCase(), arr[1].toLowerCase()); 
-            lexikon.put(arr[1].toLowerCase(), arr[0].toLowerCase()); 
+            lexikon.put(arr[0].toLowerCase(), arr[1].toLowerCase()); // Swe - Eng
+            lexikon.put(arr[1].toLowerCase(), arr[0].toLowerCase()); // Eng - Swe
 
         }
 
-        while (true) { 
-            Scanner sc = new Scanner(System.in);
+        while (true) { // Skapar en while loop för att min scanner inte ska avsluta efter att ha fått gjort sitt
+            Scanner sc = new Scanner(System.in); // Jag berättar för min scanner att den ska ta in info ifrån consolen
 
-            String input = sc.next().toLowerCase(); 
-            lexikon.get(input); 
-            if (lexikon.get(input) == null) { 
-                System.out.println("ERROR: Tyvärr, ditt ord finns inte i den Svensk-Engelska ordlistan!"); 
+            String input = sc.next().toLowerCase(); // Jag tar in ett värde ifrån consolen och gör detta till lowercase, att jag tar in den som lowercase, jag kallar detta värde för "input"
+            lexikon.get(input); // Jag säger till mitt lexikon att ta in ordet jag nyss skrev in i "input"
+            if (lexikon.get(input) == null) { // om den inte hittar en motsvarighet till inputordet i lexikonet så ger programmet svaret "null", jag ändrar svaret till nedan
+                System.out.println("ERROR: Tyvärr, ditt ord finns inte i den Svensk-Engelska ordlistan!"); // ändrar null-felmeddelandet till "ERROR: Tyvärr, ditt ord finns inte i den Svensk-Engelska ordlistan!"
 
 
-            } else { 
-                System.out.println("Ordet " + input + " översätts till " + lexikon.get(input)); 
+            } else { // Om det inte blir ett felmeddelande så skriver jag ut följande
+                System.out.println("Ordet " + input + " översätts till " + lexikon.get(input)); // "Ordet MIN-INPUT översätts till ORDETS-MOTSVARIGHET-I-ENGELSKA
 
             }
 
@@ -48,22 +58,7 @@ public class function {
 
     }
 
-    public int testing() throws FileNotFoundException
-    {
-        File file = new File("C:\\Users\\kakak_000\\IdeaProjects\\untitled5\\src\\com\\company\\lexikon.txt");
-        Scanner in = new Scanner(file);
 
-
-
-        while(in.hasNextLine())
-        {
-            in.nextLine();
-            words++;
-
-            return words;
-        }
-
-    }
 }
 
 
