896516821c0b0311d982dcac5405cb10328f389e
Kim <kakakan@live.se>
2014-10-30 21:49:38 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,46 @@
 package com.company;
 
-import java.io.IOException;
+import java.io.*;
 
+import java.util.HashMap;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        function function = new function(); // Här säger jag till att den ska läsa ifrån Person.java
-        function.counter();
+public class function {
+    String file = "C:\\Users\\kakak_000\\IdeaProjects\\untitled5\\src\\com\\company\\lexikon.txt";
+    String line;
+    public void counter() throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        HashMap<String, String> lexikon;
+        lexikon = new HashMap<String, String>();
 
+        while ((line = br.readLine()) != null) {
+            String[] arr = line.split("\\s+");
+            arr[0] = arr[0].toLowerCase();
+            arr[1] = arr[1].toLowerCase();
+            lexikon.put(arr[0].toLowerCase(), arr[1].toLowerCase());
+            lexikon.put(arr[1].toLowerCase(), arr[0].toLowerCase());
+        }
+        br.close();
+
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Skriv in ett värde i consolen: ");
+            String input = sc.next().toLowerCase();
+            lexikon.get(input);
+            if (lexikon.get(input) == null) {
+                System.out.println("ERROR: Tyvärr, ditt ord finns inte i den Svensk-Engelska ordlistan!");
+            }
+            else {
+                System.out.println("Ordet " + input + " översätts till " + lexikon.get(input));
+            }
+        }
     }
-
 }
+
+
+
+
+
 
 
 
